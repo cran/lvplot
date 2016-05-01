febc1db4fc1f3cebf609b16440eab54045494caf
Hadley Wickham <hadley@rstudio.com>
2016-05-01 18:56:01 +0000

    version 0.2.0

--- R/draw.r
+++ R/draw.r
@@ -1,59 +1,104 @@
-# LV summary table
-# Create letter value summary table
-# 
-# @arguments numeric vector
-# @arguments quantiles to compute
-# @arguments number of letter statistics
-# @arguments list of outliers
-# @arguments depth of the corresponding LV statistic (i.e. how far from the outside do we have to go into the sorted data values?)
-# @arguments significance level
-# @value letter.val: letter value statistic, distinguishes between upper and lower LV statistic for all statistics but the median
-# @value conf.int: confidence interval of corresponding letter value statistic
-# @value out: list of defined outliers
-# @keyword internal 
-outputLVplot <- function(x,qu,k,out,depth,alpha) {
-  low <- depth - floor(0.5 *sqrt(2*depth-1) * qnorm(alpha+(1-alpha)/2))
-  high <- depth + ceiling(0.5 *sqrt(2*depth-1) * qnorm(alpha+(1-alpha)/2))
+#' Compute letter value summary table.
+#'
+#' @param x numeric vector
+#' @param qu quantiles to compute
+#' @param out binary vector of outliers (\code{TRUE} for outlier,
+#'   \code{FALSE} otherwise)
+#' @inheritParams determineDepth
+#' @keywords internal
+#' @return
+#'  \item{letter.val}{letter value statistic, distinguishes between upper and
+#'    lower LV statistic for all statistics but the median}
+#'  \item{conf.int}{confidence interval of corresponding letter value
+#'    statistic}
+#'  \item{out}{list of defined outliers}
+#' @noRd
+outputLVplot <- function(x,qu,k,out,alpha) {
   n <- length(x)
-  LV <- cbind(depth,lower=qu[k:1],upper=qu[k+1:k])
-  y <- sort(x)
-  conf <- cbind(c(y[rev(low[-1])],y[n-high]),c(y[rev(high[-1])],y[n-low]))
-  colnames(conf) <- c(paste((1-alpha)/2*100,"%",sep=""),paste((alpha+(1-alpha)/2)*100,"%",sep=""))
-  if (k > 1) {
-    which <- (((k-1):1 + (6-k)) %% 26) + 1
-    row.names(LV) <- c('M',toupper(letters[which]))
-    row.names(conf) <- c(paste(toupper(letters[rev(which)]),"l",sep=""),'M',paste(toupper(letters[which]),"u",sep=""))
-  } 
-  if (k == 1) {
-    row.names(LV) <- 'M'
-    row.names(conf) <- 'M'
-  }
 
-  result <- list(letter.val = LV, conf.int= conf,outliers = x[out])
+  depth <- getDepth(k,n)
+
+  LV <- cbind(depth,lower=qu[k:1],upper=qu[k-1+1:k])
+  conf <- confintLV(x, k, alpha=alpha)
+
+  dimnames <- nameLV(k)
+  row.names(LV) <- dimnames[[1]]
+  row.names(conf) <- dimnames[[2]]
+
+  result <- list(letter.val = LV, conf.int= conf,outliers = which(out))
   return(result)
 }
 
-# Draw an LV plot 
-# Draw a letter value boxplot
-# 
-# @arguments x positions
-# @arguments y positions
-# @arguments number of letter value statistics used
-# @arguments out: outliers
-# @arguments quantiles
-# @arguments display horizontally (TRUE) or vertically (FALSE)
-# @arguments vector of colours to use
-# @keyword internal
-drawLVplot <- function(x,y,k,out,qu,horizontal,col,...) {
-  if (horizontal) { 
-	points(x[out],rep(y,length(x[out])),pch=8)		
-	# draw boxes:
-	for (i in 1:k) 
-		rect(qu[i], y+i/(2*k),qu[2*k-i+1], y-i/(2*k), col=col[i])
+#' Draw an LV plot.
+#'
+#' @param x x positions
+#' @param y y positions
+#' @param k number of letter value statistics used
+#' @param out indices of outliers
+#' @param qu quantiles
+#' @param horizontal display horizontally (TRUE) or vertically (FALSE)
+#' @param col vector of colours to use
+#' @param border vector of colours to use
+#' @param width maximum height/width of box
+#' @param width.method one of 'linear', 'height' or 'area'. Methods 'height' and 'area' ensure that these dimension are proportional to the number of observations within each box.
+#' @keywords internal
+drawLVplot <- function(x,y,k,out,qu,horizontal,col, border="grey50", width=0.9, width.method = "linear", median.col, ...) {
+  i <- seq_len(k)
+  y <- rep(y, length(x))
+
+  lower <- i[-k]
+  upper <- rev(seq_len(k-1) + k)
+  col <- rev(col[i])
+
+  if (width.method=="linear") {
+    offset <- width/2* c(lower / k, 1, rev(lower) / k)
+  } else {
+    if (width.method=="area") {
+      areas <- .5*c(2^(lower-k), rev(2^(lower-k)))
+      height <- diff(qu)
+      # offset is half the width
+      offset <- width/2*c(areas[lower]/height[lower], .9*min(1, 1/width),
+                          rev(areas[upper-1])/rev(height[upper-1]))
+    } else {
+      if (width.method=="height") {
+        height <- 2*c(2^(lower-k), rev(2^(lower-k)))
+        offset <- width/2*c(height[lower], 1, rev(height[upper-1]))
+      } else stop("parameter width.method is not specified. Use 'linear', 'area', or 'height'")
+    }
+  }
+
+
+  if (horizontal) {
+    points(x[out], y[out], pch = 1, cex=0.7)
+
+    # bottom rectangles:
+    rect(qu[lower], y[lower] + offset[lower],
+         qu[lower+1], y[lower] - offset[lower], col = col, border=border)
+    # top rectangles:
+    rect(qu[upper-1], y[upper] + offset[upper],
+         qu[upper], y[upper] - offset[upper], col = col, border=border)
+
+    # draw the median as a line
+    med = length(lower) + 1
+    oldpar <- par()
+    par(lwd=2*oldpar$lwd)
+    lines(x=c(qu[med],qu[med]),
+          y=c(y[med]-offset[med], y[med]+offset[med]), col=median.col)
+    par(lwd=oldpar$lwd)
   } else { # draw vertical plot
-	points(rep(y,length(x[out])),x[out],pch=8)						 
-	# draw boxes:
-	for (i in 1:k) 
-		rect(y+i/(2*k),qu[i], y-i/(2*k), qu[2*k-i+1], col=col[i])
+    points(y[out], x[out], pch = 1, cex=0.7)
+    # bottom rectangles:
+    rect(y[lower] + offset[lower], qu[lower],
+         y[lower] - offset[lower], qu[lower+1], col = col, border=border)
+    # top rectangles:
+    rect(y[upper] + offset[upper], qu[upper-1],
+         y[upper] - offset[upper], qu[upper], col = col, border=border)
+
+    med = length(lower)+1
+    oldpar <- par()
+    par(lwd=2*oldpar$lwd)
+    lines(x=c(y[med]-offset[med], y[med]+offset[med]),
+          y=c(qu[med],qu[med]), col=median.col)
+    par(lwd=oldpar$lwd)
   }
 }
--- R/lvplot.r
+++ R/lvplot.r
@@ -1,204 +1,213 @@
-# LV box plot
-# An extension of standard boxplots which draws k letter statistics
-# 
-# This is a generic method so please see specific methods for details.
-#
-# @seealso \code{\link{LVboxplot.formula}}, \code{\link{LVboxplot.numeric}}
-# @keyword internal
+#' @importFrom grDevices chull col2rgb hsv rgb rgb2hsv
+#' @importFrom graphics abline axis box grid lines par plot points polygon rect
+#' @importFrom stats qnorm
+NULL
+
+#' Side-by-side LV boxplots with base graphics
+#'
+#' An extension of standard boxplots which draws k letter statistics.
+#' Conventional boxplots (Tukey 1977) are useful displays for conveying rough
+#' information about the central 50\% of the data and the extent of the data.
+#'
+#' For moderate-sized data sets (\eqn{n < 1000}), detailed estimates of tail
+#' behavior beyond the quartiles may not be trustworthy, so the information
+#' provided by boxplots is appropriately somewhat vague beyond the quartiles,
+#' and the expected number of ``outliers'' and ``far-out'' values for a
+#' Gaussian sample of size \eqn{n} is often less than 10 (Hoaglin, Iglewicz,
+#' and Tukey 1986). Large data sets (\eqn{n \approx 10,000-100,000}) afford
+#' more precise estimates of quantiles in the tails beyond the quartiles and
+#' also can be expected to present a large number of ``outliers'' (about
+#' \eqn{0.4 + 0.007 n}).
+#'
+#' The letter-value box plot addresses both these shortcomings: it conveys
+#' more detailed information in the tails using letter values, only out to the
+#' depths where the letter values are reliable estimates of their
+#' corresponding quantiles (corresponding to tail areas of roughly
+#' \eqn{2^{-i}}); ``outliers'' are defined as a function of the most extreme
+#' letter value shown. All aspects shown on the letter-value boxplot are
+#' actual observations, thus remaining faithful to the principles that
+#' governed Tukey's original boxplot.
+#'
+#' @family letter-value boxplots
+#' @export
 LVboxplot <- function(x, ...) UseMethod("LVboxplot",x)
 
-# Side-by-side LV boxplots
-# An extension of standard boxplots which draws k letter statistics
-# 
-# Conventional boxplots (Tukey 1977) are useful displays for conveying rough information
-# about the central 50\% of the data and the extent of the data.
-# 
-# For moderate-sized data sets ($n < 1000$), detailed estimates of tail behavior beyond
-# the quartiles may not be trustworthy, so the information provided by boxplots is
-# appropriately somewhat vague beyond the quartiles, and the expected number of
-# ``outliers'' and ``far-out'' values for a Gaussian sample of size $n$ is often less
-# than 10 (Hoaglin, Iglewicz, and Tukey 1986). Large data sets ($n \approx
-# 10,000-100,000$) afford more precise estimates of quantiles in the tails beyond the
-# quartiles and also can be expected to present a large number of ``outliers'' (about 0.4
-# + 0.007$n$).
-# 
-# The letter-value box plot addresses both these shortcomings: it conveys more detailed
-# information in the tails using letter values, only out to the depths where the letter
-# values are reliable estimates of their corresponding quantiles (corresponding to tail
-# areas of roughly $2^{-i}$); ``outliers'' are defined as a function of the most extreme
-# letter value shown. All aspects shown on the letter-value boxplot are actual
-# observations, thus remaining faithful to the principles that governed Tukey's original
-# boxplot.
-# 
-# @arguments the formula has to be of the form $y \tilde x$, where $x$ is a qualitative variable. The values of $y$ will be split into groups according to their values on $x$ and separate letter value box plots of $y$ are drawn side by side in the same display.
-# @arguments significance level, if neither \code{k} nor \code{perc} is specified, \code{alpha} is used to determine how many letter values are to be used.
-# @arguments percentage of data points to be shown individually (as outliers) outside the letter-value boxes. \code{perc} is only used, if \code{k} is not specified.  If used, $k$ is determined in such a way, that confidence intervals around each letter value statistics will not include neighboring letter value statistics at a significance level of \code{alpha}.
-# @arguments number of letter statistics to compute and draw
-# @arguments if defined, aim for \code{perc} percent outliers
-# @arguments display horizontally (TRUE) or vertically (FALSE)
-# @arguments specify base colour to use
-# @arguments unused
-# @keyword hplot
-# @seealso \code{\link{LVboxplot.numeric}}
-#X n <- 10
-#X oldpar <- par()
-#X par(mfrow=c(4,2), mar=c(3,3,3,3))
-#X for (i in 1:4) {
-#X 	x <- rexp(n*10^i)
-#X 	boxplot(x,col="grey", horizontal=TRUE)
-#X 	title(paste("Exponential, n=",length(x)))
-#X 	LVboxplot(x,col="grey", xlab="")
-#X }
-LVboxplot.formula <- function(formula,alpha=0.95, k=NULL, perc=NULL,horizontal=TRUE,col="grey",...) {
-    deparen <- function(expr) {
-        while (is.language(expr) && !is.name(expr) && deparse(expr[[1]]) == 
-            "(") expr <- expr[[2]]
-        expr
-    }
-    bad.formula <- function() stop("invalid formula; use format y ~ x")
-    bad.lengths <- function() stop("incompatible variable lengths")
-    
-    formula <- deparen(formula)
-    if (!inherits(formula, "formula")) 
-        bad.formula()
-    z <- deparen(formula[[2]])
-    x <- deparen(formula[[3]])
-    rhs <- deparen(formula[[3]])
-    if (is.language(rhs) && !is.name(rhs) && (deparse(rhs[[1]]) == 
-        "*" || deparse(rhs[[1]]) == "+")) {
-        bad.formula()
-    }
-    z.name <- deparse(z)
-    z <- eval(z,  parent.frame())
-    x.name <- deparse(x)
-    x <- eval(x,  parent.frame())
-	setx <- sort(unique(x))
-    src.k <- k 
-    src.col <- col 
 
+#' @param formula a plotting formula of the form \code{y ~ x}, where \code{x}
+#'   is a string or factor. The values of \code{y} will be split into groups
+#'   according to their values on \code{x} and separate letter value box plots
+#'   of \code{y} are drawn side by side in the same display.
+#' @param xlab x axis label
+#' @param ylab y axis label
+#' @param bg background colour
+#' @param median.col colour of the line for the median
+#' @inheritParams determineDepth
+#' @inheritParams drawLVplot
+#' @param ... passed onto \code{\link{plot}}
+#' @export
+#' @rdname LVboxplot
+#' @examples
+#' n <- 10
+#' oldpar <- par()
+#' par(mfrow=c(4,2), mar=c(3,3,3,3))
+#' for (i in 1:4) {
+#'   x <- rexp(10 ^ (i + 1))
+#'   boxplot(x, col = "grey", horizontal = TRUE)
+#'   title(paste("Exponential, n = ", length(x)))
+#'   LVboxplot(x, col = "grey", xlab = "")
+#' }
+#' par(mfrow=oldpar$mfrow, mar=oldpar$mar)
+#'
+#' with(ontime, LVboxplot(sqrt(TaxiIn + TaxiOut) ~ UniqueCarrier, horizontal=FALSE))
+LVboxplot.formula <- function(formula,alpha=0.95, k=NULL, perc=NULL, horizontal=TRUE, xlab=NULL, ylab=NULL, col="grey30", bg="grey90", width=0.9, width.method = "linear", median.col="grey10", ...) {
+  deparen <- function(expr) {
+    while (is.language(expr) && !is.name(expr) && deparse(expr[[1]]) ==
+        "(") expr <- expr[[2]]
+    expr
+  }
+  bad.formula <- function() stop("invalid formula; use format y ~ x")
+  bad.lengths <- function() stop("incompatible variable lengths")
 
-    pt <- 1
-	if (horizontal) {
-	  plot(z,rep(pt,length(z)),ylim=c(0.5,length(setx)+.5),ylab="",axes=FALSE,type="n",...)
-	  box()
-	  axis(1)
-	  axis(2,at=1:length(setx),labels=as.character(setx))
-	} else {
-	  plot(rep(pt,length(z)),z,xlim=c(0.5,length(setx))+0.5, xlab="", axes=FALSE, type="n", ...)
-	  box()
-	  axis(2)
-	  axis(1,at=1:length(setx),labels=as.character(setx))
-	}
-	
-	result <- list(length(setx))
-    for (i in setx) {
-       xx <- z[x==i]
-	   n <- length(xx)
-	   k <- determineDepth(n,src.k,alpha,perc) 
+  formula <- deparen(formula)
+  if (!inherits(formula, "formula"))
+      bad.formula()
+  z <- deparen(formula[[2]])
+  x <- deparen(formula[[3]])
+  rhs <- deparen(formula[[3]])
+  if (is.language(rhs) && !is.name(rhs) && (deparse(rhs[[1]]) ==
+    "*" || deparse(rhs[[1]]) == "+")) {
+    bad.formula()
+  }
+  z.name <- deparse(z)
+  z <- eval(z,  parent.frame())
+  x.name <- deparse(x)
+  x <- eval(x,  parent.frame())
+  setx <- sort(unique(x))
+  src.k <- k
+  src.col <- col
 
-	   if (! is.na(src.col)) { 
-	   		#col <- c(brewer.pal(k-1,"Blues"),"Black") # break dependency of ColorBrewer package
-	   		
-	   		colrgb <- col2rgb(src.col)
-	   		colhsv <- rgb2hsv(colrgb)
-			if (colhsv[2,1] == 0) {
-	   			val <- seq(0.9,colhsv[3,1], length.out=k)
-	   			colrgb <- col2rgb(hsv(colhsv[1,1], colhsv[2,1], val))
-			} else {
-	   			sat <- seq(0.1,colhsv[2,1], length.out=k)
-	   			colrgb <- col2rgb(hsv(colhsv[1,1], sat, colhsv[3,1]))
-			}
-	   		col <- rgb(colrgb[1,],colrgb[2,],colrgb[3,], maxColorValue=255) 	
-	   	}
-	   else { col <- rep("grey",k) }
-	   
-	 # compute letter values based on depth  
-	   depth    <- rep(0,k)
-	   depth[1] <- (1 + n)/2
-	   if (k > 1) {
-		 for (j in 2:k) {depth[j] <- (1 + floor(depth[j-1]))/2 } 
-	   }
-	   
-	   y <- sort(xx)
-	   d <- c(rev(depth),n-depth+1)
-	   qu <- (y[floor(d)] + y[ceiling(d)])/2 	
-		 # floor and ceiling is the same for .0 values
-		 # .5 values yield average of two neighbours
-	   
-	 # determine outliers
-	   out <- (xx<qu[1]) | (xx>qu[2*k])            
-		  
-	   drawLVplot(xx,pt,k,out,qu,horizontal,col,...)
-	   result[[pt]] <- outputLVplot(xx,qu,k,out,depth,alpha)      
-	   pt <- pt+1
-    }
-    invisible(as.list(result))
+  # Drop missing values with a message
+  missing <- is.na(x) | is.na(z)
+  if (any(missing)) {
+    message("Dropping ", sum(missing), " missing values.")
+    x <- x[!missing]
+    z <- z[!missing]
+  }
+
+  pt <- 1
+  if (horizontal) {
+    if (is.null(xlab)) xlab=z.name
+    if (is.null(ylab)) ylab=x.name
+    plot(z, rep(pt, length(z)), ylim=c(0.5, length(setx)+.5), ylab=ylab,
+      xlab=xlab,  axes=FALSE, type="n", ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = bg)
+    box()
+    axis(1)
+    axis(2,at=1:length(setx), labels=as.character(setx))
+    grid(lty=1, col="white", ny=NA)
+    abline(h=1:length(setx), col="white")
+  } else {
+    if (is.null(ylab)) ylab=z.name
+    if (is.null(xlab)) xlab=x.name
+    plot(rep(pt, length(z)), z, xlim=c(0, length(setx))+0.5,  xlab=xlab,
+      ylab=ylab,  axes=FALSE,  type="n",  ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = bg)
+    box()
+    axis(2)
+    axis(1,at=1:length(setx),labels=as.character(setx))
+    grid(lty=1, col="white")
+    abline(v=1:length(setx), col="white")
+  }
+
+  # compute one set of colours for the rectangles
+  xtable <- table(x, useNA="ifany")
+  kmax <- determineDepth(max(xtable),src.k,alpha,perc)
+  if (length(src.col)==1) col <- color_scale(src.col, kmax)
+  if (length(src.col)==0) col <- rep("grey",kmax)
+  #       if (length(src.col > 1)) col <- src.col
+
+  boxes <- split(z, x)
+  result <- vector("list", length(boxes))
+  for (i in seq_along(boxes)) {
+   xx <- boxes[[i]]
+   n <- length(xx)
+   k <- determineDepth(n, src.k, alpha, perc)
+
+   # compute letter values and outliers
+   qu <- calcLV(xx, k)
+   out <- xx < min(qu) | xx > max(qu)
+
+   drawLVplot(xx,i,k,out,qu,horizontal,col=col[(kmax-k) +1:k], width=width, width.method=width.method, median.col=median.col, ...)
+   result[[pt]] <- outputLVplot(xx,qu,k,out,alpha)
+   pt <- pt+1
+  }
+  invisible(as.list(result))
 }
 
-# Single LV boxplot
-# Produces a single lettervalue boxplot for the specified data.
-# 
-# @arguments alpha level for significance level: alpha 100\% confidence intervals do not touch neighboring LV statistics
-# @arguments number of letter statistics to compute and draw
-# @arguments if defined, aim for \code{perc} percent outliers
-# @arguments display horizontally (TRUE) or vertically (FALSE)
-# @arguments specify base colour to use
-# @arguments unused
-# @keyword hplot
-# @seealso \code{\link{LVboxplot.formula}}
-LVboxplot.numeric <- function(x,alpha=0.95, k=NULL, perc=NULL,horizontal=TRUE,col="grey",...) {
-# extension of standard boxplots
-# draws k letter statistics
+#' @param x numeric vector of data
+#' @export
+#' @rdname LVboxplot
+LVboxplot.numeric <- function(x,alpha=0.95, k=NULL, perc=NULL, horizontal=TRUE, xlab=NULL, ylab=NULL, col="grey30", bg="grey90", width=0.9, width.method = "linear", median.col="grey10", ...) {
+  x.name <- as.list(match.call())[-1]$x
+  x <- eval(x,  parent.frame())
 
   n <- length(x)
-  k <- determineDepth(n,k,alpha,perc) 
-  src.col <- col 
+  k <- determineDepth(n,k,alpha,perc)
+  src.col <- col
+  if (length(src.col)==1) col <- color_scale(src.col, k)
+  if (length(src.col)==0) col <- rep("grey",k)
+  #  if (length(src.col > 1)) col <- src.col
 
-  if (! is.na(src.col)) { #col <- c(brewer.pal(k-1,"Blues"),"Black") 
- 	   		
-	   		colrgb <- col2rgb(src.col)
-	   		colhsv <- rgb2hsv(colrgb)
-			if (colhsv[2,1] == 0) {
-	   			val <- seq(0.9,colhsv[3,1], length.out=k)
-	   			colrgb <- col2rgb(hsv(colhsv[1,1], colhsv[2,1], val))
-			} else {
-	   			sat <- seq(0.1,colhsv[2,1], length.out=k)
-	   			colrgb <- col2rgb(hsv(colhsv[1,1], sat, colhsv[3,1]))
-			}
-	   		col <- rgb(colrgb[1,],colrgb[2,],colrgb[3,], maxColorValue=255) 	
-  }
-  else { col <- rep("grey",k) }
-  
-# compute letter values based on depth  
-  depth    <- rep(0,k)
-  depth[1] <- (1 + n)/2
-  if (k > 1) {
-    for (j in 2:k) {depth[j] <- (1 + floor(depth[j-1]))/2 } 
-  }
-  
-  y <- sort(x)
-  d <- c(rev(depth),n-depth+1)
-  qu <- (y[floor(d)] + y[ceiling(d)])/2 	
-    # floor and ceiling is the same for .0 values
-  	# .5 values yield average of two neighbours
-  
-# determine outliers
-  out <- (x<qu[1]) | (x>qu[2*k])               
+  qu <- calcLV(x, k)
+
+  # determine outliers
+  out <- ((x<min(qu)) + (x>max(qu))) > 0
   if (k < 1) out <- x
 
   pt <- 0.5
   if (horizontal) {
-    plot(x,rep(pt,length(x)),ylim=c(pt-0.5,pt+0.5),ylab="",axes=FALSE,type="n",...)
-	box()
-	axis(1)
-  } else {
-	plot(rep(pt,length(x)),x,xlim=c(pt-0.5,pt+0.5), xlab="", axes=FALSE, type="n", ...)
-	box()
-	axis(2)
-  }
-     
-  drawLVplot(x,pt,k,out,qu,horizontal,col,...)
+    if (is.null(xlab)) xlab=x.name
+    if (is.null(ylab)) ylab=""
 
-  result <- outputLVplot(x,qu,k,out,depth,alpha)
+    plot(x, rep(pt, length(x)), ylim=c(pt-0.5, pt+0.5), ylab=ylab, xlab=xlab,
+      axes=FALSE, type="n", ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = bg)
+    box()
+    grid(lty=1, col="white", ny=NA)
+    abline(h=pt, col="white")
+    axis(1)
+  } else {
+    if (is.null(ylab)) ylab=x.name
+    if (is.null(xlab)) xlab=""
+    plot(rep(pt, length(x)), x, xlim=c(pt-0.5, pt+0.5),  ylab=ylab, xlab=xlab,
+      axes=FALSE,  type="n",  ...)
+    rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = bg)
+    box()
+    grid(lty=1, col="white", ny=NA)
+    abline(v=pt, col="white")
+    axis(2)
+  }
+
+  drawLVplot(x,pt,k,out,qu,horizontal,col=col,width=width, width.method=width.method, median.col=median.col, ...)
+
+  result <- outputLVplot(x,qu,k,out,alpha)
   invisible(result)
 }
+
+color_scale <- function(src.col, k) {
+# starting colour src.col
+# number of colours
+  colrgb <- col2rgb(src.col)
+  colhsv <- rgb2hsv(colrgb)
+  if (colhsv[1,1] == 0) {
+    sat <- colhsv[2,1]
+  } else {
+    sat <- seq(0.1,colhsv[2,1], length.out=k)
+  }
+
+  val <- seq(0.9,colhsv[3,1], length.out=k)
+  colrgb <- col2rgb(hsv(colhsv[1,1], sat, val))
+  col <- rgb(colrgb[1,],colrgb[2,],colrgb[3,], maxColorValue=255)
+  col
+}
+
